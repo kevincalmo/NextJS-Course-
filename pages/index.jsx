ece64c3b8ecf2231284da55c93172c82fd71dfc9
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-10-21 23:04:16 -0400

    Add linking to antoher page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,22 @@
 //my root page '/'
+import Link from "next/link";
 import React from "react";
 
 const Homepage = () => {
     return (
         <>
             <h1>HomePage</h1>
-            </>
+            <ul>
+                <li>
+                    {/* Remplace la balise a et permet de ne pas recharger la page
+                    on reste sur une single page application 
+                    et on a une meilleure expérience utilisateur */}
+                    <Link href='/news'>
+                        All News
+                    </Link>
+                </li>
+            </ul>
+        </>
     )
 }
 
