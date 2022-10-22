ba0c4ccef67119eb90d25b41f22917e6747d4698
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-10-21 22:35:53 -0400

    Create static pages

---
+++ pages/news/something-important.jsx
@@ -0,0 +1,12 @@
+// our-domain.com/news/something-important
+import React from "react";
+
+const DetailPage = () => {
+    return(
+        <>
+        <h1>The details pages</h1>
+        </>
+    )
+}
+
+export default DetailPage;
